662c1e5b3525c1cee2b4f1b6a2067b472ab694af
Vlad G <127021567+Umojan@users.noreply.github.com>
2025-04-11 20:04:09 +0600

    Tickets

--- django_airport/flights/api.py
+++ django_airport/flights/api.py
@@ -1,5 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import FlightViewSet
+from .views import FlightViewSet, search_flights
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'', FlightViewSet, basename='flight')
+router.register(r'flights', FlightViewSet, basename='flight')
+
+urlpatterns = router.urls + [
+    path('search/', search_flights, name='search-flights'),
+]
