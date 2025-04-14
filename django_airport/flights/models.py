4e0314ba03ac43cb406a328528cd5b3198420fc3
Vlad G <127021567+Umojan@users.noreply.github.com>
2025-04-14 19:49:28 +0600

    Translate flights

--- django_airport/flights/models.py
+++ django_airport/flights/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 class Flight(models.Model):
-    is_arrival = models.BooleanField()
-    airline = models.CharField(max_length=50)
+    is_arrival = models.BooleanField()  # True: прибытие, False: вылет
+    airline = models.CharField(max_length=100)
     flight_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
+    arrival_city = models.CharField(max_length=100)
+    arrival_city_en = models.CharField(max_length=100, blank=True)
+    arrival_city_kg = models.CharField(max_length=100, blank=True)
     time = models.DateTimeField()
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.flight_code} - {self.airline} - {'Arrival' if self.is_arrival else 'Departure'}"
+        return f"{self.airline} {self.flight_code} ({self.arrival_city})"
 
     class Meta:
         db_table = 'flights_schedule'
