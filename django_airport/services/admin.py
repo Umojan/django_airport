d49fee9a384948fc0c70335b103b2599811cd2d4
Vlad G <127021567+Umojan@users.noreply.github.com>
2025-04-14 19:24:38 +0600

    Translate services

--- django_airport/services/admin.py
+++ django_airport/services/admin.py
@@ -3,6 +3,8 @@ from .models import Service
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'service_type', 'location', 'open_time', 'close_time')
-    list_filter = ('service_type', 'location')
-    search_fields = ('name', 'description', 'location')
+    list_display = (
+        'id', 'name_ru', 'name_en', 'name_ky', 'service_type', 'location', 'open_time', 'close_time'
+    )
+    list_filter = ('service_type',)
+    search_fields = ('name_ru', 'name_en', 'name_ky', 'description_ru', 'description_en', 'description_ky')
--- django_airport/services/models.py
+++ django_airport/services/models.py
@@ -8,15 +8,31 @@ class Service(models.Model):
         ('simcard', 'SIM-card'),
     ]
 
-    name = models.CharField(max_length=100)
+    # Основное название и переводы
+    name_ru = models.CharField("Название (русский)", max_length=100)
+    name_en = models.CharField("Название (английский)", max_length=100, blank=True)
+    name_ky = models.CharField("Название (кыргызский)", max_length=100, blank=True)
+
     service_type = models.CharField(max_length=10, choices=SERVICE_TYPES)
-    description = models.TextField(blank=True)
+
+    # Описание и переводы
+    description_ru = models.TextField("Описание (русский)", blank=True)
+    description_en = models.TextField("Описание (английский)", blank=True)
+    description_ky = models.TextField("Описание (кыргызский)", blank=True)
+
     location = models.CharField(max_length=100)
     open_time = models.TimeField()
     close_time = models.TimeField()
-    
+
     image = models.ImageField(upload_to='service_images/', blank=True, null=True)
 
-
     def __str__(self):
-        return f"{self.name} ({self.get_service_type_display()})"
+        # Выбираем название в зависимости от текущего языка
+        from django.utils import translation
+        current_language = translation.get_language()
+        if current_language == 'en' and self.name_en:
+            return f"{self.name_en} ({self.get_service_type_display()})"
+        elif current_language == 'ky' and self.name_ky:
+            return f"{self.name_ky} ({self.get_service_type_display()})"
+        else:
+            return f"{self.name_ru} ({self.get_service_type_display()})"
